429f833c5398473ea532c644708de4ac60d18018
Yuliia Hladyshkevych <yulia.hladyshkevych@gmail.com>
2024-02-05 21:06:00 +0200

    Update OrderForm with start_date and end_date

--- orders/forms.py
+++ orders/forms.py
@@ -1,34 +1,53 @@
 from django import forms
-from django.core.exceptions import ValidationError
 from django.utils import timezone
 
 from orders.models import Order
 
 
 class OrderForm(forms.ModelForm):
+    start_date = forms.DateTimeField(
+        widget=forms.DateTimeInput(attrs={"type": "datetime-local"}),
+        input_formats=["%Y-%m-%dT%H:%M"],
+    )
+    end_date = forms.DateTimeField(
+        input_formats=["%d.%m.%Y %H:%M"],
+        widget=forms.DateTimeInput(attrs={"type": "datetime-local"}),
+    )
+
     class Meta:
         model = Order
         fields = [
             "user_email",
             "user_name",
-            "start_timestamp",
-            "end_timestamp",
+            "start_date",
+            "end_date",
         ]
 
     def clean(self):
         cleaned_data = super().clean()
-        end_timestamp = self.cleaned_data.get("end_timestamp")
-        start_timestamp = self.cleaned_data.get("start_timestamp")
+        end_date = self.cleaned_data.get("end_date")
+        start_date = self.cleaned_data.get("start_date")
 
-        if end_timestamp and start_timestamp:
-            if start_timestamp <= timezone.now().timestamp():
-                raise ValidationError(
-                    "Start timestamps must be later than the current time."
+        if end_date and start_date:
+            if start_date <= timezone.now():
+                raise forms.ValidationError(
+                    "Start date must be later than the current time."
                 )
 
-            if end_timestamp <= start_timestamp:
-                raise ValidationError(
-                    "End timestamp must be later than start timestamp."
+            if end_date <= start_date:
+                raise forms.ValidationError(
+                    "End date must be later than start date."
                 )
+
+            cleaned_data["start_timestamp"] = int(start_date.timestamp())
+            cleaned_data["end_timestamp"] = int(end_date.timestamp())
 
         return cleaned_data
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        instance.start_timestamp = self.cleaned_data.get("start_timestamp")
+        instance.end_timestamp = self.cleaned_data.get("end_timestamp")
+        if commit:
+            instance.save()
+        return instance
