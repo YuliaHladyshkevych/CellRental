8f955baafa0e5f3f35cc5fc1cb530dfab24ec81c
Yuliia Hladyshkevych <yulia.hladyshkevych@gmail.com>
2024-02-05 14:16:21 +0200

    Created OrderSerializer

---
+++ orders/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from orders.models import Order
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = (
+            "id",
+            "slug",
+            "start_timestamp",
+            "end_timestamp",
+            "user_email",
+            "user_name",
+            "cell_id",
+            "reminded",
+            "created_at"
+        )
