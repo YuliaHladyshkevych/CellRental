5c36c26dc92af93c1db621273d60cf0fe2d36433
Yuliia Hladyshkevych <yulia.hladyshkevych@gmail.com>
2024-02-06 12:22:17 +0200

    Created a celery task that will remind you about the end of the rental

--- cell_rental/celery.py
+++ cell_rental/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cell_rental.settings")
 
@@ -8,6 +9,13 @@ app = Celery("cell_rental")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    "send-send_reminder_email_task": {
+        "task": "orders.tasks.send_reminder_email_task",
+        "schedule": crontab(minute="*/15"),
+    },
+}
 
 
 @app.task(bind=True, ignore_result=True)
--- orders/tasks.py
+++ orders/tasks.py
@@ -1,3 +1,5 @@
+import time
+
 from celery import shared_task
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
@@ -25,3 +27,32 @@ def send_email_task(order_id):
         recipient_list=to_email,
         html_message=html_message,
     )
+
+
+@shared_task
+def send_reminder_email_task():
+    orders = Order.objects.filter(
+        end_timestamp__gte=time.time(),
+        end_timestamp__lte=time.time() + 30 * 60,
+        reminded=False,
+    )
+    for order in orders:
+        subject = f"Order #{order.id}"
+        message = ""
+        from_email = settings.EMAIL_HOST_USER
+        to_email = [order.user_email]
+        html_message = render_to_string(
+            "orders/reminder_email_template.html",
+            {"order": order, "BASE_URL": settings.BASE_URL},
+        )
+
+        send_mail(
+            subject=subject,
+            message=message,
+            from_email=from_email,
+            recipient_list=to_email,
+            html_message=html_message,
+        )
+
+        order.reminded = True
+        order.save()
